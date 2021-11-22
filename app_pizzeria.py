b1f69a594c3c7f01408013b510612a7807f69152
rociobrq <94622563+rociobrq@users.noreply.github.com>
2021-11-22 19:30:06 -0300

    Update app_pizzeria.py

--- app_pizzeria.py
+++ app_pizzeria.py
@@ -1,36 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Importar Tkinter
 import tkinter as tk
 from tkinter import Menu
 from tkinter import LabelFrame, Label, Frame
 from tkinter import Button
 from PIL import Image, ImageTk
 
-#
+# Se importa database
 from db_pizzeria import DB_pizzeria
-#from graficos import graficos
 from cliente import cliente
-#from equipo import equipo
+
 class App:
     def __init__(self, db):
+        # Se actualiza atributo con la database
         self.db = db
 
-        # Main window
+        # Creación de ventana principal
         self.root = tk.Tk()
-        #self.master = master
+        self.root.geometry("700x500")
 
-        # Algunas especificaciones de tamaño y título de la ventana
-        self.root.geometry("700x500") # se define tamaño de la ventana
-        self.root.title(" APP Pizzeria l'italiano ") # se añade el titulo de la app
-        self.root.config(bg="light cyan") # se añade color al fondo de la ventana
+        # Título de la ventana
+        self.root.title("APP Pizzería l'italiano")
+        # Se añade color al fondo de la ventana
+        self.root.config(bg="light cyan")
 
-        # se crea mediante self._ el menu de opciones utiles para el usuario
-        self.__crearMenu()
         self.__crearBotones()
+        # Se crea mediante self._ el menu de opciones utiles para el usuario
+        self.__crearMenu()
         self.__agregarImagenInicial()
 
         # Empieza a correr la interfaz.
         self.root.mainloop()
 
-        # Se crea una funcion que nos permitira tener el menu
+    def __cerrarVentana(self):
+        self.root.destroy()
+
+    def __crearBotones(self):
+        # Se construyen algunos de los botones que son parte de la app
+        boton_pedido = Button(self.root, text="Pedido", width=20).place(x=15, y=20)
+        boton_cliente = Button(self.root, text="Cliente", command = self.__mostrar_cliente, width=20).place(x=15, y=80)
+        boton_pizza = Button(self.root, text="Pizza", width=20).place(x=15, y=140)
+        boton_repartidor = Button(self.root, text="Repartidor", width=20).place(x=15, y=200)
+        boton_vehiculo = Button(self.root, text="Vehiculo", width=20).place(x=15, y=260)
+        boton_salir = Button(self.root, text="Salir", command=self.root.destroy, width=20).place(x=350, y=400)
+
     def __crearMenu(self):
         menu_opciones = Menu(self.root)
         self.root.config(menu=menu_opciones)
@@ -50,8 +65,6 @@ class App:
         # dentro del boton de AYUDA, existira uno que detalle para guiar al usuario
         help_menu.add_command(label = "Ayuda")
 
-
-        # esta funcion permite que se carge la imagen de inicio de la app
     def __agregarImagenInicial(self):
         frame = LabelFrame(self.root, text="", relief=tk.FLAT)
         # se define la ubicacion del frame
@@ -67,35 +80,16 @@ class App:
         label.image = photo
         label.pack()
 
-    def __crearBotones(self):
-        # se construyen algunos de los botones que son parte de la app
-        boton_pedido = Button(self.root, text="Pedido", width=20).place(x=15, y=20)
-        boton_cliente = Button(self.root, text="Cliente", width=20, command=self.__mostrar_cliente).place(x=15, y=80)
-        boton_pizza = Button(self.root, text="Pizza", width=20).place(x=15, y=140)
-        boton_repartidor = Button(self.root, text="Repartidor", width=20).place(x=15, y=200)
-        boton_vehiculo = Button(self.root, text="Vehiculo", width=20).place(x=15, y=260)
-
-        # Se define la funcionalidad para que boton salir funcione
-        # se genera un boton para salir (cerrar app)
-        boton_salir = Button(self.root, text="Salir", command=self.root.destroy, width=20).place(x=350, y=400)
-
-    # muestra ventana equipos.
     def __mostrar_cliente(self):
         cliente(self.root, self.db)
-
-    # muestra ventana jugadores.
-    #def __mostrar_jugadores(self, button):
-    #    jugador(self.root, self.db)
-
-    #def __graficos(self, button):
-    #    graficos(self.root, self.db)
 
 def main():
     # Conecta a la base de datos
     db = DB_pizzeria()
 
-    # La app xD
+    # App que toma como parámetro la database
     App(db)
 
 if __name__ == "__main__":
     main()
+
