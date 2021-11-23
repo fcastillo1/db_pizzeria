c31930b76b427c85df705a242ec0d16f7e534246
fcastillo1 <94079587+fcastillo1@users.noreply.github.com>
2021-11-23 12:30:50 -0300

    cambios

--- app_pizzeria.py
+++ app_pizzeria.py
@@ -20,7 +20,6 @@ class App:
         # Creación de ventana principal
         self.root = tk.Tk()
         self.root.geometry("700x500")
-
         # Título de la ventana
         self.root.title("APP Pizzería l'italiano")
         # Se añade color al fondo de la ventana
@@ -34,21 +33,20 @@ class App:
         # Empieza a correr la interfaz.
         self.root.mainloop()
 
-    def __cerrarVentana(self):
-        self.root.destroy()
 
     def __crearBotones(self):
         # Se construyen algunos de los botones que son parte de la app
-        boton_pedido = Button(self.root, text="Pedido", width=20).place(x=15, y=20)
-        boton_cliente = Button(self.root, text="Cliente", command = self.__mostrar_cliente, width=20).place(x=15, y=80)
-        boton_pizza = Button(self.root, text="Pizza", width=20).place(x=15, y=140)
-        boton_repartidor = Button(self.root, text="Repartidor", width=20).place(x=15, y=200)
-        boton_vehiculo = Button(self.root, text="Vehiculo", width=20).place(x=15, y=260)
-        boton_salir = Button(self.root, text="Salir", command=self.root.destroy, width=20).place(x=350, y=400)
+        boton_pedido = Button(self.root, text="Pedido", width=20, bg='snow', fg='black').place(x=15, y=20)
+        boton_cliente = Button(self.root, text="Cliente", command = self.__mostrar_cliente, width=20 , bg='snow', fg='black').place(x=15, y=80)
+        boton_pizza = Button(self.root, text="Pizza", width=20 , bg='snow', fg='black').place(x=15, y=140)
+        boton_repartidor = Button(self.root, text="Repartidor", width=20 , bg='snow', fg='black').place(x=15, y=200)
+        boton_vehiculo = Button(self.root, text="Vehiculo", width=20 , bg='snow', fg='black').place(x=15, y=260)
+        boton_r = Button(self.root, text="Salir", command=self.root.destroy, width=20, bg='red', fg='white').place(x=350, y=400)
 
     def __crearMenu(self):
         menu_opciones = Menu(self.root)
         self.root.config(menu=menu_opciones)
+
         # se contruye el menu de la informacion con su color
         information_menu = Menu(menu_opciones, tearoff = 0, bg ="white")
         menu_opciones.add_cascade(label = "Informacion", menu=information_menu)
@@ -56,7 +54,7 @@ class App:
         information_menu.add_command(label = "Acerca de App")
         # se genera un espacio
         information_menu.add_separator()
-        # se genera un boton salir, que pirmitira destuir la ventana
+        # se genera un boton r, que pirmitira destuir la ventana
         information_menu.add_command(label = "Salir", command=self.root.destroy)
 
         # se contruye el menu de ayuda con su color
@@ -92,4 +90,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
