00f3962e42a65fee244d06d56d705fdea374d49b
rociobrq <94622563+rociobrq@users.noreply.github.com>
2021-12-14 15:09:45 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -94,6 +94,7 @@ class cliente:
             self.data = data
 
     def __insertar_cliente(self):
+        # Llamada a la clase que permite realizar la inserción de un cliente
         insertar_cliente(self.db, self)
 
     def __eliminar_cliente(self):
@@ -103,8 +104,13 @@ class cliente:
         if ((self.treeview.focus() != "") and
             (messagebox.askyesno(message = texto_msj, title = "Alerta") == True)):
 
+            # Consulta para realizar la eliminación de un cliente
             opEliminar = "DELETE FROM cliente where rut_clie = %(rut_clie)s"
+
+            # Ejecución de la eliminación en la base de datos
             self.db.run_sql(opEliminar, {"rut_clie": self.treeview.focus()}, "D")
+
+            # Se actualiza el treeview de la ventana de cliente
             self.llenar_treeview_cliente()
 
     def __modificar_cliente(self):
@@ -120,6 +126,8 @@ class cliente:
 
             # Se consulta en la tabla cliente por el rut del registro a modificar
             mod_select = self.db.run_select_filter(opModificar, {"rut": self.treeview.focus()})[0]
+
+            # Se llama a la clase para modificar el cliente con los datos actuales
             modificar_cliente(self.db, self, mod_select)
 
 class insertar_cliente:
@@ -210,16 +218,19 @@ class insertar_cliente:
         opInsert = """INSERT cliente (rut_clie, nom_clie, ape_clie, tel_clie, dir_clie, id_ciudad)
                 values (%(rut)s, %(nombre)s, %(apellido)s, %(telefono)s, %(direccion)s, %(ciudad)s)"""
 
-        # Se ejecuta consulta
+        # Se ejecuta consulta en la base de datos
         self.db.run_sql(opInsert, {"rut": self.rut.get(),"nombre": self.nombre.get(),
         "apellido": self.apellido.get(), "telefono": self.telefono.get(), "direccion": self.direccion.get(),
             "ciudad": self.ids[self.combo.current()]}, "I")
 
+        # Se cierra la ventana de inserción de clientes
         self.insert_datos.destroy()
+        # Se actualiza el treeview de la ventana principal
         self.padre.llenar_treeview_cliente()
 
 class modificar_cliente:
     def __init__(self, db, padre, mod_select):
+        # Ventana principal de cliente corresponde a ventana padre
         self.padre = padre
         self.db = db
         self.mod_select = mod_select
@@ -236,7 +247,7 @@ class modificar_cliente:
         self.insert_datos.resizable(width=0, height=0)
 
     def __config_label(self):
-        # Definición de entradas de texto para la clase cliente
+        # Etiquetas para señalar el texto que debe ingresarse
         rut_lab = tk.Label(self.insert_datos, text = "Rut: ")
         rut_lab.place(x = 10, y = 10, width = 120, height = 20)
         nom_lab = tk.Label(self.insert_datos, text = "Nombre: ")
@@ -251,7 +262,8 @@ class modificar_cliente:
         ciu_lab.place(x = 10, y = 160, width = 120, height = 20)
 
     def __config_entry(self):
-        # Se obtiene texto para ingresar clientes
+        # Se obtiene texto para ingresar clientes (rut, nombre, apellido, teléfono,
+        # dirección)
         self.rut = tk.Entry(self.insert_datos)
         self.rut.place(x = 110, y = 10, width = 150, height = 20)
         self.nombre = tk.Entry(self.insert_datos)
@@ -262,22 +274,34 @@ class modificar_cliente:
         self.telefono.place(x = 110, y = 100, width = 150, height = 20)
         self.direccion = tk.Entry(self.insert_datos)
         self.direccion.place(x = 110, y = 130, width = 150, height = 20)
+
+        # Se crea un combobox para que el cliente pueda seleccionar la ciudad
         self.combo = ttk.Combobox(self.insert_datos)
         self.combo.place(x = 110, y = 160, width = 150, height= 20)
         self.combo["values"], self.ids = self.__llenar_combo()
 
+        # Se guarda el valor del rut actual del registro a modificar
         self.rut_viejo = self.mod_select[0]
+
+        # Se insertan valores actuales en las entradas de texto
         self.rut.insert(0, self.mod_select[0])
         self.nombre.insert(0, self.mod_select[1])
         self.apellido.insert(0, self.mod_select[2])
         self.telefono.insert(0, self.mod_select[3])
         self.direccion.insert(0, self.mod_select[4])
+
+        # Se inserta ciudad actual en el combobox
         self.combo.insert(0, self.mod_select[5])
         self.combo.config(state = "readonly")
 
     def __llenar_combo(self):
+        # Consulta para obtener id y nombre de ciudad
         opLlenar = "SELECT id_ciudad, nom_ciudad FROM ciudad"
+
+        # Ejecución de la consulta en la base de datos
         self.data = self.db.run_select(opLlenar)
+
+        # Se retorna nombre e id
         return [i[1] for i in self.data], [i[0] for i in self.data]
 
     def __config_button(self):
@@ -292,13 +316,18 @@ class modificar_cliente:
         btn_cancel.place(x = 210, y = 200, width = 80, height = 20)
 
     def __modificar(self):
+        # Consulta para modificar el registro de acuerdo a un rut específico
         opEdicion = """UPDATE cliente set rut_clie = %(rut)s, nom_clie = %(nombre)s,
             ape_clie = %(apellido)s, tel_clie = %(telefono)s, dir_clie = %(direccion)s,
             id_ciudad = %(ciudad)s WHERE rut_clie = %(rut_viejo)s"""
 
+        # Se ejecuta la consulta
         self.db.run_sql(opEdicion, {"rut": self.rut.get(),"nombre": self.nombre.get(),
         "apellido": self.apellido.get(), "telefono": self.telefono.get(), "direccion": self.direccion.get(),
             "ciudad": self.ids[self.combo.current()], "rut_viejo": self.rut_viejo}, "U")
 
+        # Destrucción de la ventana de modificación
         self.insert_datos.destroy()
+
+        # Se actualiza el treeview de la ventana principal de cliente
         self.padre.llenar_treeview_cliente()
